952283c89f3e6901f3366bcbff681cc57c06cacf
TRW <itgz2085@outlook.com>
2026-09-17 13:22:11 +0800

    添加超时记录

--- app/app.c
+++ app/app.c
@@ -2,6 +2,7 @@
 #include "app_cfg.h"
 //
 #include "bsp_log.h"
+#include "bsp_sys_status.h"
 #include "bsp_dwt.h"
 #include "lib_math.h"
 #include "bsp_freertos.h"
@@ -36,7 +37,10 @@ ITCM_RAM static __attribute__((noreturn)) void StartChassisTask(void *argument)
         AppChassisRun();
         dt = DWT_GetTimeUs() - start;
         if (dt > 1000 * CHASSIS_FREQ_MS)
+        {
             BSPLOG(&g_app_log, LOG_LEVEL_ERROR, "CHASSIS Task is being DELAY! dt = %llu(us)", dt);
+            BSP_ASSERT_TASK_TIMEOUT(); // 任务超时：系统状态计数 +1
+        }
     }
 }
 
